7a54057054d79968cd6a113918f6a24e09f4082b
Vadzim Matsiushonak <vadzimmatsiushonak@gmail.com>
2023-02-04 16:25:32 +0300

    1480. Running Sum of 1d Array

--- src/by/vadzim/LeetCode75/Day1/T1480/Solution.java
+++ src/by/vadzim/LeetCode75/Day1/T1480/Solution.java
@@ -3,7 +3,7 @@ package by.vadzim.LeetCode75.Day1.T1480;
 import java.util.Arrays;
 
 /**
- * Best results:
+ * Best results
  * Runtime 0 ms Beats 100%
  * Memory 41.9 MB Beats 94.49%
  *
@@ -13,14 +13,26 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {1, 1, 1, 1, 1, 1, 1, 1};
-        int[] result1 = solution.runningSum1(nums);
-        int[] result2 = solution.runningSum2(nums);
-        System.out.println(Arrays.toString(result1));
-        System.out.println(Arrays.toString(result2));
+
+        int[] result1 = solution.runningSum(new int[]{1, 2, 3, 4});
+        int[] result2 = solution.runningSum(new int[]{1, 1, 1, 1, 1});
+        int[] result3 = solution.runningSum(new int[]{3, 1, 2, 10, 1});
+
+        System.out.println(Arrays.equals(new int[]{1, 3, 6, 10}, result1) + " result1 = " + Arrays.toString(result1));
+        System.out.println(Arrays.equals(new int[]{1, 2, 3, 4, 5}, result2) + " result2 = " + Arrays.toString(result2));
+        System.out.println(
+            Arrays.equals(new int[]{3, 4, 6, 16, 17}, result3) + " result3 = " + Arrays.toString(result3));
     }
 
-    public int[] runningSum1(int[] nums) {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++)
+            nums[i] += nums[i - 1];
+        return nums;
+    }
+
+/*
+    First solution
+    public int[] runningSum(int[] nums) {
         int currentValue = 0;
         int[] output = new int[nums.length];
 
@@ -31,11 +43,6 @@ class Solution {
 
         return output;
     }
+*/
 
-    public int[] runningSum2(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1];
-        }
-        return nums;
-    }
 }
